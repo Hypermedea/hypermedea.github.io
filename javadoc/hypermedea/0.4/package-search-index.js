a273d328c7385aafb2d37715da712f7a939025df
Victor Charpenay <victor.charpenay@emse.fr>
2023-11-28 08:42:41 +0100

    add maven repo; restructure javadoc

---
+++ javadoc/hypermedea/0.4/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"h"},{"l":"org.hypermedea"},{"l":"org.hypermedea.ct"},{"l":"org.hypermedea.ct.json"},{"l":"org.hypermedea.ct.rdf"},{"l":"org.hypermedea.ct.txt"},{"l":"org.hypermedea.op"},{"l":"org.hypermedea.op.file"},{"l":"org.hypermedea.op.http"},{"l":"org.hypermedea.tools"}];updateSearchResults();
